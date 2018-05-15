7b8d523076187cd83f4966f17b428d81e6f2a38e
Andrew J. Sagegit config --global user.email ajsage@iastate.edu <ajsage@gmail.com>
2018-05-15 15:31:27 -0500

    change partitioning for real data

--- R/RealData.R
+++ R/RealData.R
@@ -3,8 +3,10 @@
 #' Function to take partitioned data, and make predictions across all folds
 #'
 #'
-#' @param PartitionedData  List of partitioned dataframes for training, test sets, and indicator of outlying observations
-#' @param folds fold on which to assess performance
+#' @param dataset  List of partitioned dataframes for training, test sets, and indicator of outlying observations
+#' @param partitions array containing indices of training and test cases
+#' @param p percentage of training cases for which to add contamination (using N(0, 5*sd(Y)))
+#' @param fold fold on which to assess performance
 #' @param ntrees  number of trees
 #' @param ndsize nodesize
 #' @param ntreestune  number of trees to use for tuning alpha
@@ -18,8 +20,18 @@
 #'        3. Number of iterations
 #'        4. Output from TuneMultifoldCV (a list of 8 items itself)
 #' @export
-Assess_Folds <- function(PartitionedData, fold, ntrees, ndsize, ntreestune, parvec, cvreps, cvfolds, tol ){
-DATA <- list(data.frame(PartitionedData[[1]][fold,,]), data.frame(PartitionedData[[2]][fold,,]), PartitionedData[[3]][fold,])
+Assess_Folds <- function(dataset, partitions, p, fold, ntrees, ndsize, ntreestune, parvec, cvreps, cvfolds, tol ){
+stdev=sd(dataset[,ncol(dataset)])
+CVTRAINind <- partitions[[1]]
+CVTESTind <- partitions[[2]]
+OutlierIndicator <- partitions[[3]]
+TRAIN <- dataset[CVTRAINind[fold,],]
+ds=rbinom(ntrain,1,p)  #determine which training cases are outliers
+e=c(rep(0, ntrain))   #initialize error vector
+e[ds==1]=rnorm(length(e[ds==1]),0,5*stdev) #contaminated errors for variance cont.
+TRAIN[,ncol(TRAIN)] <-TRAIN[,ncol(TRAIN)]+e
+TEST <- dataset[CVTESTind[fold,],]
+DATA <- list(TRAIN, TEST, OutlierIndicator)
 names(DATA[[1]])[ncol(DATA[[1]])] <- "Y"
 names(DATA[[2]])[ncol(DATA[[2]])] <- "Y"
 Preds <- Make_All_Preds(DATA=DATA, ntrees, ndsize, ntreestune, parvec, cvreps, cvfolds, tol)
@@ -51,8 +63,8 @@ return(Preds)
 
 
 Assess_Real_Data <- function(dataset, nfolds, p, ntrees, ndsize, ntreestune, parvec, cvreps, cvfolds, tol ){
-    FULLDATA <- Partition_Real_Data(dataset, nfolds=nfolds, p=p)
-    Preds <- sapply(1:nfolds, simplify="array", Assess_Folds, PartitionedData=FULLDATA, ntrees=ntrees, ndsize=ndsize,
+    cuts <- Partition_Real_Data(dataset, nfolds=nfolds)
+    Preds <- sapply(1:nfolds, simplify="array", Assess_Folds, dataset=dataset, partitions=cuts, p=0 , ntrees=ntrees, ndsize=ndsize,
            ntreestune=ntreestune, parvec=parvec, cvreps=cvreps, cvfolds=cvfolds, tol=tol  )
   return(Preds)
 }
